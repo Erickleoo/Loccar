666650330927ffb069db8bfe5bf1fb2f57a404a9
ErickLeoo <ericleonardo_2012@hotmail.com>
2022-10-03 19:35:51 -0300

    pastas do projeto

--- loccar/src/app/app.module.ts
+++ loccar/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { CarrosComponent } from './components/carros/carros.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { LocadoraComponent } from './components/locadora/locadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    PerfilComponent,
+    CarrosComponent,
+    ReservasComponent,
+    LocadoraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
