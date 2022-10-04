b258e36b449d7cfa8be31f609766cd8834ddb23f
Robert Oliveira <86692878+Robert-Oliveira@users.noreply.github.com>
2022-10-04 12:09:41 -0300

    html e css component carro

--- loccar/src/app/app.module.ts
+++ loccar/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,7 +42,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatButtonModule,MatToolbarModule
+    MatButtonModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
